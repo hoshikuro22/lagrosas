da779c0035d6394ea8ec07edb31f52f2f659e737
Tipsy Lagrosas <superlaggerxz@gmail.com>
2023-02-02 18:42:11 +0800

    edit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Experience from "./components/Experience";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Portfolio from "./components/Portfolio";
-import Test from "./components/Test";
 
 
 function App() {
@@ -16,7 +15,6 @@ function App() {
  <Portfolio />
  <Experience />
  <Contact />
- <Test />
 
  
   </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -29,10 +29,7 @@ const NavBar = () => {
       id: 5,
       link: 'contact'
     },
-    {
-      id: 6,
-      link: 'test'
-    },
+    
    
    
   ]
